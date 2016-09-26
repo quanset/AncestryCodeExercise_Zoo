600c33fabdcb6b79eb68c9cf8d30e9c5de845c88
quanset <p.quans@yahoo.com>
2016-09-25 20:10:37 -0600

    Adding files

---
+++ src/IZI/FeedInventoryManagementSystem.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package IZI;
+
+/**
+ * @author Ken Fultz
+ *
+ */
+public class FeedInventoryManagementSystem {
+
+	/**
+	 * 
+	 */
+	public FeedInventoryManagementSystem() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
